6e74ce2ff5f575110b9825ca12e537295a51443b
gio <giotnhaduh@gmail.com>
2022-01-12 14:40:23 +0700

    'backendSignin'

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/haa", {
   // useNewUrlParser: true,
   // userUnifiedTopology: true,
