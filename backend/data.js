c404593136efbd49fb397b236a6552e6da6b24d2
gio <giotnhaduh@gmail.com>
2022-01-12 13:40:07 +0700

    'backendSampleUser'

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "tez",
+      email: "tez@gmail.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+    },
+    {
+      name: "tez1",
+      email: "tez1@gmail.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       _id: "1",
